71651d5e9f1771b8ab588598301af5b431393e95
RatedMForMuZhiE <1014112@student.sae.edu.au>
2020-02-26 11:13:43 +1000

    Changes to my scene, testing out some UI things

---
+++ Assets/Scripts/SceneAndExternalsManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneAndExternalsManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void OpenWebsite(string url)
+    {
+        Application.OpenURL(url);
+    }
+}
