8d813d1a1988584db11a5e3f199f689e8579a300
RatedMForMuZhiE <1014112@student.sae.edu.au>
2020-03-13 21:12:31 +1000

    Added test character and made a new build

---
+++ Assets/Scripts/TestScripts/TestCorkboardLinkScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestCorkboardLinkScript : MonoBehaviour
+{
+    public string myUrl;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadWebPage()
+    {
+        Debug.Log(myUrl);
+        Application.OpenURL(myUrl);
+    }
+
+    public void FinishedEditing(InputField myInput)
+    {
+        myUrl = myInput.text;
+            //.ToString(); 
+    }
+}
