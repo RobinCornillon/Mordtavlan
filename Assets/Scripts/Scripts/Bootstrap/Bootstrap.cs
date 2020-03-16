8942c50bb4b3fdbbd5b03da49520d01e00b99ed7
RatedMForMuZhiE <1014112@student.sae.edu.au>
2020-03-16 15:37:45 +1000

    Added a bootstrap scene where we can keep all audio.

---
+++ Assets/Scripts/Scripts/Bootstrap/Bootstrap.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Bootstrap
+{
+
+    [RuntimeInitializeOnLoadMethod]
+    private static void OnRuntimeLoad()
+    {
+        if (SceneManager.GetActiveScene().name != "Bootstrap")
+        {
+            SceneManager.LoadScene("Bootstrap", LoadSceneMode.Additive);
+
+        }
+    }
+}
